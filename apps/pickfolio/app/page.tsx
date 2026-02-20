4ab175d07948074e2598ea2ecb4e7f761cc47d3a
Robert Porancea <robert.porancea.pfa@gmail.com>
2026-02-20 18:46:58 +0200

    deploy test

--- apps/pickfolio/app/page.tsx
+++ apps/pickfolio/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { APP_NAME } from '@pickfolio/server';
-import { greet } from '@pickfolio/ui';
 
 export default function Home() {
     return (
@@ -9,7 +8,7 @@ export default function Home() {
                 <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
                     <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                        Welcome to {APP_NAME} {greet('John')}
+                        Welcome to {APP_NAME}
                     </h1>
                     <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
                         Looking for a starting point or more instructions? Head over to{' '}
